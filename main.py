513e3b5588866bcbe2c1cf90def9101f8603d110
salman haydar <salmanhaydar@salmans-MacBook-Pro.local>
2019-08-19 18:46:01 +0600

    some modules have added

--- main.py
+++ main.py
@@ -38,40 +38,6 @@ except:
 
 extractor = FeatureExtractor(elmo_embedding)
 
-# def get_feature_vector_train(sentences,embedding=None):
-#   if embedding == None:
-#     raise Exception('Please pass flair embedding object.')
-#   else:
-#     sentence_vectors = []
-#     for sent in sentences:
-#       s = Sentence(sent)
-#       embedding.embed(s)
-      
-#       temp_placeholder = []
-#       for token in s:
-#         temp_placeholder.append(token.embedding.numpy().reshape(1,-1))
-        
-#       sentence_vectors.append(np.mean(temp_placeholder,axis=0))
-      
-      
-#     return np.mean(sentence_vectors,axis=0)
-
-# def get_feature_vector_infer(sentence,embedding=None):
-#   if embedding == None:
-#     raise Exception('Please pass flair embedding object.')
-#   else:
-    
-#     sent = Sentence(sentence)
-#     embedding.embed(sent)
-
-#     temp_placeholder = []
-#     for token in sent:
-#       temp_placeholder.append(token.embedding.numpy().reshape(1,-1))
-
-#     sent_embd = np.mean(temp_placeholder,axis=0)
-
-#     return sent_embd
-
 @app.route("/getintent",methods=["GET","POST"])
 def getIntent():
 
