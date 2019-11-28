727be7ea17cbf336a09044fa4a478dbdef629146
Salman Haydar <salmanhaydar@Salmans-MacBook-Pro.local>
2019-11-28 11:38:18 +0600

    Response type added

--- features.py
+++ features.py
@@ -1,18 +1,23 @@
 import numpy as np
+import pickle
+import os
 
 
 class FeatureExtractor:
 
-    def __init__(self,wv=None):
-        self.wv = wv
+    def __init__(self,embedding=None):
+        self.wv = embedding
+        
+        self.pruning_factor_alpha = 0.5
 
 
-    def get_feature_vector_train(self,sentences):
+    def get_feature_vector_train(self,sentences,path):
 
         if self.wv == None:
             raise Exception('Please pass the KeyedVectors object.')
 
         else:
+            self.store_vocab(sentences,path)
             sentence_vectors = []
             for sent in sentences:
 
@@ -26,27 +31,67 @@ class FeatureExtractor:
                         pass
 
                 if len(temp_placeholder)>0:
-                    sentence_vectors.append(np.mean(temp_placeholder,axis=0))
+                    
+#                     sentence_vectors.append(np.mean(temp_placeholder,axis=0))
+                    summation = np.zeros(1024,dtype=np.float32)
+                    for vector in temp_placeholder:
+                        summation = np.add(summation,vector)
+                    sent_embd = summation
+                    sentence_vectors.append(sent_embd.reshape(1,-1))
 
             return np.mean(sentence_vectors,axis=0).reshape(1,-1)
 
 
-    def get_feature_vector_infer(self,sentence):
+    def get_feature_vector_infer(self,sentence,path):
 
         if self.wv == None:
-            raise Exception('Please pass KeyedVector object.')
+            raise Exception('Please pass the KeyedVectors object.')
 
         else:
+
+            with open (path, 'rb') as fp:
+                vocab_repo = pickle.load(fp)
+
             temp_placeholder = []
             for token in sentence.split():
                 try:
-                    temp_placeholder.append(self.wv[token])
+                    if token in vocab_repo:
+                        
+#                         unit_vector = self.wv[token]/np.linalg.norm(self.wv[token])
+                        temp_placeholder.append(self.wv[token])
+                    else:
+#                         pass
+#                         reduced_vector = self.wv[token]*self.pruning_factor_alpha
+#                         unit_vector = reduced_vector/np.linalg.norm(reduced_vector)
+                        temp_placeholder.append(self.wv[token]*self.pruning_factor_alpha)
                 except KeyError:
                     pass
-
             if len(temp_placeholder)>0:
-                sent_embd = np.mean(temp_placeholder,axis=0)
+#                 sent_embd = np.mean(temp_placeholder,axis=0)
+                summation = np.zeros(1024,dtype=np.float32)
+                for vector in temp_placeholder:
+                    summation = np.add(summation,vector)
+                sent_embd = summation
                 return sent_embd.reshape(1,-1)
             else:
                 return np.zeros(1024,dtype=np.float32).reshape(1,-1)
-            
+
+    def store_vocab(self,sentences,path):
+
+        vocab_repo = []
+        for sentence in sentences:
+            for word in sentence.split(" "):
+                vocab_repo.append(word)
+                try:
+                    for sim_word in self.wv.most_similar(word,topn=3):
+                        vocab_repo.append(sim_word[0])
+                except KeyError:
+                    pass
+        if os.path.exists(path):
+            with open (path, 'rb') as fp:
+                itemlist = pickle.load(fp)
+            for word in itemlist:
+                vocab_repo.append(word)
+
+        with open(path,'wb') as f:
+            pickle.dump(list(set(vocab_repo)),f)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify,session, render_template,redirect, url_for
+from flask import Response
 import numpy as np
 import pandas as pd
 from sklearn.utils import shuffle
@@ -12,6 +13,7 @@ import json
 import ast
 import re
 import os
+from scipy.spatial import distance
 from flask_cors import CORS
 from sklearn.metrics.pairwise import cosine_similarity
 from gensim.models import Word2Vec
@@ -45,25 +47,36 @@ def getIntent():
     bot_id = request.args.get("botID")
     sentence = request.args.get("sentence").lower()
     # print(request.args)
-    query_vec = extractor.get_feature_vector_infer(sentence)
+    
     # print(query_vec.shape)
 
     if(os.path.exists(os.path.join(BOT_BASE,bot_id))):
-    
+      
       into_home = os.path.join(BOT_BASE,bot_id)
+
+      vocab_home = os.path.join(into_home,"vocab_repo")
+      vocab_file_path = os.path.join(vocab_home,bot_id+".pickle")
+      query_vec = extractor.get_feature_vector_infer(sentence,vocab_file_path)
+
       if(os.path.exists(os.path.join(into_home,"trained_data"))):
         intent_files = [ f.split(".")[0] for f in os.listdir(os.path.join(into_home,"trained_data"))]
+
         result_dic = {}
+        confidence = {}
+        confidence_cosine = {}
+
         print(intent_files)
         trained_data_repo = os.path.join(into_home,"trained_data")
         # print(os.listdir(os.path.join(into_home,"trained_data")))
         for intent in intent_files:
             intent_vec = np.load(os.path.join(trained_data_repo,intent+".npy"))
-            result_dic[intent] = str(cosine_similarity(query_vec,intent_vec)[0][0])
+            result_dic[intent] = distance.euclidean(query_vec,intent_vec)
+            confidence[intent] = str(distance.euclidean(query_vec,intent_vec))
+            confidence_cosine[intent] = str(cosine_similarity(query_vec,intent_vec)[0][0])
         print(result_dic)
-        res = max(result_dic, key=result_dic.get)
+        res = min(result_dic, key=result_dic.get)
 
-        if float(result_dic[res]) < 0.50:
+        if float(result_dic[res]) > 20.0:
           payloads["intent"] = "unknown"
           payloads["Status"] = "success"
           payloads["Message"] = "Couldn't understand properly"
@@ -72,20 +85,21 @@ def getIntent():
           payloads["Status"] = "success"
           payloads["Message"] = "null"
 
-        payloads["confidence"] = result_dic
+        payloads["confidence"] = confidence
+        payloads['confidence_cosine'] = confidence_cosine
 
-        return json.dumps(payloads)
+        return Response(json.dumps(payloads),mimetype='application/json')
 
       else:
         
-        return json.dumps({"Status":"failed","Message":'Error!! Your Bot is not yet trained',"intent":"null","confidence":"null"})
+        return Response(json.dumps({"Status":"failed","Message":'Error!! Your Bot is not yet trained',"intent":"null","confidence":"null"}),mimetype='application/json')
 
     else:
       
-      return json.dumps({"Status":"failed","Message":'Error!! No Bot found with this Bot ID',"intent":"null","confidence":"null"}), 404
+      return Response(json.dumps({"Status":"failed","Message":'Error!! No Bot found with this Bot ID',"intent":"null","confidence":"null"}),status=404,mimetype='application/json')
 
   else:
-      return json.dumps({"Status":"failed","Message":'This method is not allowed',"intent":"null","confidence":"null"}), 405
+      return Response(json.dumps({"Status":"failed","Message":'This method is not allowed',"intent":"null","confidence":"null"}),status=405,mimetype='application/json')
 
 
 @app.route("/addexample",methods=["GET","POST","PUT"])
@@ -103,15 +117,15 @@ def addexample():
       #if the file exist insert the data to the existing data repository
       df = pd.DataFrame({"sentence":[sentence],"intent":[intent]})
       df.to_csv(os.path.join(training_data_home,bot_id+".csv"),mode="a",header=False,index=False)
-      return json.dumps({"Status":"success","Message":'Added Successfully',"intent":"null","confidence":"null"})
+      return Response(json.dumps({"Status":"success","Message":'Added Successfully',"intent":"null","confidence":"null"}),mimetype='application/json')
     else:
       #else create a new data repository with the name <bot_id.csv>
       df = pd.DataFrame({"sentence":[sentence],"intent":[intent]})
       df.to_csv(os.path.join(training_data_home,bot_id+".csv"),columns=df.columns,index=False)
-      return json.dumps({"Status":"success","Message":'Added Successfully',"intent":"null","confidence":"null"})
+      return Response(json.dumps({"Status":"success","Message":'Added Successfully',"intent":"null","confidence":"null"}),mimetype='application/json')
   else:
     #for the methods other than PUT
-    return json.dumps({"Status":"failed","Message":'This method is not allowed',"intent":"null","confidence":"null"}), 405
+    return Response(json.dumps({"Status":"failed","Message":'This method is not allowed',"intent":"null","confidence":"null"}),mimetype='application/json',status=405)
 
 @app.route("/train",methods=["GET","POST"])
 def train():
@@ -120,6 +134,8 @@ def train():
     BOT_HOME = os.path.join(BOT_BASE,bot_id)
     trained_data_home = os.path.join(BOT_HOME,"trained_data")
     training_data_home = os.path.join(BOT_HOME,"training_data")
+    vocab_home = os.path.join(BOT_HOME,"vocab_repo")
+    vocab_file_path = os.path.join(vocab_home,bot_id+".pickle")
 
     if os.path.exists(os.path.join(training_data_home,bot_id+".csv")):
       df = pd.read_csv(os.path.join(training_data_home,bot_id+".csv"))
@@ -129,18 +145,18 @@ def train():
         try:
           filtered_group = grouped_df.get_group(intent)
           sentences = list(filtered_group.sentence)
-          feature_vector = extractor.get_feature_vector_train(sentences)
+          feature_vector = extractor.get_feature_vector_train(sentences,vocab_file_path)
           np.save(os.path.join(trained_data_home,intent+".npy"),feature_vector)
         except:
-          return json.dumps({"Status":"failed","Message":'INTERNAL ERROR!',"intent":"null","confidence":"null"}),500
+          return Response(json.dumps({"Status":"failed","Message":'INTERNAL ERROR!',"intent":"null","confidence":"null"}),status=500,mimetype='application/json')
 
-      return json.dumps({"Status":"success","Message":'Training has completed',"intent":"null","confidence":"null"})
+      return Response(json.dumps({"Status":"success","Message":'Training has completed',"intent":"null","confidence":"null"}),mimetype='application/json')
 
     else:
-      return json.dumps({"Status":"failed","Message":'You have no data to train',"intent":"null","confidence":"null"}), 404
+      return Response(json.dumps({"Status":"failed","Message":'You have no data to train',"intent":"null","confidence":"null"}),status=404,mimetype='application/json')
     
   else:
-    return json.dumps({"Status":"failed","Message":'This method is not allowed',"intent":"null","confidence":"null"}), 405
+    return Response(json.dumps({"Status":"failed","Message":'This method is not allowed',"intent":"null","confidence":"null"}),status= 405,mimetype='application/json')
 
 @app.route("/createprofile",methods=["GET","POST"])
 def createprofile():
@@ -153,12 +169,13 @@ def createprofile():
           BOT_HOME = os.path.join(BOT_BASE,bot_id)
           os.mkdir(BOT_HOME+"/training_data")
           os.mkdir(BOT_HOME+"/trained_data")
+          os.mkdir(BOT_HOME+"/vocab_repo")
 
-          return json.dumps({"Status":"success","Message":'Bot Profile has created successfully',"intent":"null","confidence":"null"})
+          return Response(json.dumps({"Status":"success","Message":'Bot Profile has created successfully',"intent":"null","confidence":"null"}),mimetype='application/json')
         except:
-          return json.dumps({"Status":"failed","Message":'cannot create the profile',"intent":"null","confidence":"null"})
+          return Response(json.dumps({"Status":"failed","Message":'cannot create the profile',"intent":"null","confidence":"null"}),mimetype='application/json')
       except:
-        return json.dumps({"Status":"failed","Message":'cannot create the profile',"intent":"null","confidence":"null"})
+        return Response(json.dumps({"Status":"failed","Message":'cannot create the profile',"intent":"null","confidence":"null"}),mimetype='application/json')
       # if r==0:
       #  
       #   BOT_HOME = os.path.join(BOT_BASE,bot_id)
@@ -172,7 +189,7 @@ def createprofile():
       #   return json.dumps('cannot create the profile [r]')
     else:
       
-      return json.dumps({"Status":"failed","Message":'Bot with this ID is already exists',"intent":"null","confidence":"null"})
+      return Response(json.dumps({"Status":"failed","Message":'Bot with this ID is already exists',"intent":"null","confidence":"null"}),mimetype='application/json')
 
 @app.route("/",methods=["GET","POST"])
 def index():
@@ -181,4 +198,4 @@ def index():
     # return json.dumps('please see the api documentation.. https://docs.google.com/document/d/1twwLx2g315XpymM60ia7XZMF7ve-lTvm0IrbgfFtErg/edit?usp=sharing')
 
 if __name__=="__main__":
-  app.run(host="0.0.0.0",port=5656)
+  app.run(host="0.0.0.0",port=5000,debug=True)
